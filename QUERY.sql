200dddc9c383ce2610da985aadb6a5f0ef12f7a2
Joy Barua <joybarua3360@gmail.com>
2026-06-16 20:02:30 +0600

    all query done

--- QUERY.sql
+++ QUERY.sql
@@ -46,22 +46,42 @@ INSERT INTO bookings(booking_id, user_id, match_id, seat_number, payment_status,
 (505, 3, 102, 'C-20', 'Pending', 120.00);
 
 
-//Query-1
+--Query-1
 SELECT match_id, fixture, base_ticket_price FROM matches
 WHERE tournament_category = 'Champions League' AND match_status = 'Available';
 
-//Query-2
+--Query-2
 SELECT user_id, full_name, email FROM users
 WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
 
-//Query-3
+--Query-3
 SELECT booking_id, user_id, match_id,
 COALESCE (payment_status, 'Action Required') AS systematic_status
 FROM bookings 
 WHERE payment_status IS NULL;
 
 
-//Query-4
+--Query-4
 SELECT booking_id, full_name, fixture, total_cost FROM bookings
 INNER JOIN users ON bookings.user_id = users.user_id
 INNER JOIN matches ON bookings.match_id = matches.match_id;
+
+--Query-5
+SELECT users.user_id, users.full_name, bookings.booking_id
+FROM users
+LEFT JOIN bookings ON users.user_id = bookings.user_id
+ORDER BY users.user_id;
+
+
+--Query-6
+SELECT booking_id, match_id, total_cost 
+FROM bookings
+WHERE total_cost > (SELECT AVG(total_cost) FROM bookings);
+
+
+--Query-7
+SELECT match_id, fixture, base_ticket_price
+FROM matches
+ORDER BY base_ticket_price DESC
+LIMIT 2
+OFFSET 1;
